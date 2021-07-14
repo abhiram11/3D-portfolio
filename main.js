d4266bffbd4814e10d83b5a09fc63833a7ec1bf9
abhiram11 <abhiramsatpute@gmail.com>
2021-07-14 18:37:52 +0530

    added everything, now updating css

--- main.js
+++ main.js
@@ -10,21 +10,6 @@ import * as THREE from "three";
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 
-function addStar() {
-  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-  const material = new THREE.MeshStandardMaterial( {color: 0xffffff})
-  const star = new THREE.Mesh(geometry, material)
-
-  //how to randomly generate the pos of each star?
-  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100)); //(-100, 100)
-  
-  star.position.set(x,y,z);
-  scene.add(star)
-}
-
-//now create the star
-Array(200).fill().forEach(addStar)
-
 
 const scene = new THREE.Scene();
 
@@ -70,6 +55,72 @@ scene.add(lightHelper, gridHelper)
 
 const controls = new OrbitControls(camera, renderer.domElement) //listens to dom events on mouse, and update camera position accordingly
 
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial( {color: 0xffffff})
+  const star = new THREE.Mesh(geometry, material)
+
+  //how to randomly generate the pos of each star?
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100)); //(-100, 100)
+  
+  star.position.set(x,y,z);
+  scene.add(star)
+}
+
+//now create the star
+Array(200).fill().forEach(addStar)
+
+// we can also track load %, so if too large size we can add loading bar till it completes!
+const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+scene.background = spaceTexture;
+
+//my avatar! concept used: texture mapping!
+const abhiTexture = new THREE.TextureLoader().load('profilepic.jpg');
+const abhi = new THREE.Mesh(
+  new THREE.BoxGeometry(3, 3, 3), //cube like
+  new THREE.MeshBasicMaterial({map: abhiTexture})
+)
+
+scene.add(abhi)
+
+
+// adding that moon
+const moonTexture = new THREE.TextureLoader('moon.jpg');
+const normalTexture = new THREE.TextureLoader('normal.jpg')
+
+const moon = new THREE.Mesh (
+  new THREE
+  .SphereGeometry(3, 32, 32),
+  new Three.MeshBasicMaterial( {
+    map: moonTexture,
+    normalMap : normalTexture
+  })
+)
+
+scene.add(moon)
+moon.position.z = 30
+moon.position.setX(-10) //they both do the same thing, .z = 10 or .setz(10)
+
+//this function is event handler for camera
+function moveCamera() {
+  //currently scrolled to
+  const t = document.getBoundingClientRect().top;
+
+  moon.rotation.x += 0.05;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05;
+
+  abhi.rotation.y += 0.01;
+  abhi.rotation.z += 0.01;
+
+  camera.position.x = t * -0.0002;
+  camera.position.y = t * -0.0002;
+  camera.position.z = t * -0.01;
+  
+}
+
+document.body.onscroll = moveCamera
 
 //now draw the scene, aka render
 // renderer.render(scene, camera);
